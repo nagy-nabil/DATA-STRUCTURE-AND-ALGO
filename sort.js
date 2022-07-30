c9993f8c42965674740b88e29140ff4d20532c73
Nagy Nabil <nagynabil651@gmail.com>
2022-07-30 15:23:01 +0200

    quick sort js

--- sort.js
+++ sort.js
@@ -23,9 +23,33 @@ class Sort{
         }
         return newarr;
     }
+    static selectionREC(arr){
+        return [].concat(1)
+    }
+
+    static QSort(arr){
+        if(arr.length<2)
+        return arr
+        // else if(arr.length ==2){
+        //     if(arr[0] > arr[1])
+        //     [arr[0], arr[1]] = [arr[1], arr[0]];
+        //     return arr;
+        // }
+        else{
+            let pivot=arr[0];
+            // let left=arr.slice(1).filter(itm => itm <= pivot) , right=arr.slice(1).filter(itm => itm > pivot);
+            let left=[], right=[]
+            arr.slice(1).forEach(itm => {
+                itm <= pivot?left.push(itm):right.push(itm);
+            });
+            // console.log(left, right)
+            return [...Sort.QSort(left),pivot,...Sort.QSort(right)]
+        }
+    }
 }
 
-let arr=[93,675,354,45,38,7456]
-let newarr=Sort.selectionSort(arr)
-console.log(newarr)
-console.log(arr)
+let arr=[3,93,675,354,45,38,7456,93]
+console.log(Sort.QSort(arr))
+// let newarr=Sort.selectionSort(arr)
+// console.log(newarr)
+// console.log(Sort.selectionREC(arr))
